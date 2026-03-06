3c5eec1562f64c17f3ca77e14b9bd96f5cd753fe
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-06 21:38:57 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int t) {
+    int answer = 0;
+    for(int i=0;i<t;i++)
+        n*=2;
+    answer = n;
+    return answer;
+}
