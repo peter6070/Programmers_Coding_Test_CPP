f18f58c37244aa6a93061609df6674e30da1f586
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-11 14:17:05 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -4,13 +4,9 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    vector<int> answer;
-    int odd=0, even =0;
+    vector<int> answer(2,0);
     for(int n : num_list){
-        if(n%2==0) even++;
-        else odd++;
+        answer[n%2]++;
     }
-    answer.push_back(even);
-    answer.push_back(odd);
     return answer;
 }
