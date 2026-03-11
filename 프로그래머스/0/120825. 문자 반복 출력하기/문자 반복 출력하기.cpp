7c7b834689fa2ac305116ad177a59c481db2e11e
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-11 14:28:34 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 string solution(string my_string, int n) {
     string answer="";
-    for(int c=0;c<my_string.size();c++){
-        for(int i=0;i<n;i++)
-            answer+=my_string[c];
+    // for(int c=0;c<my_string.size();c++){
+    //     for(int i=0;i<n;i++)
+    //         answer+=my_string[c];
+    // }
+    for(const auto c : my_string){
+        answer+=string(n,c);
     }
     return answer;
 }
