fbc49dab5b6451d50570b31b83ffb17406d8aeed
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-12 13:40:31 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -1,18 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 vector<int> solution(vector<int> emergency) {
-    vector<int> answer(emergency.size(),0);
-    vector<int> copy = emergency;
-    sort(copy.begin(), copy.end(), greater<int>());
-    for(int i=0;i<emergency.size();i++){
-        auto index = find(copy.begin(), copy.end(),emergency[i]);
-        answer[i] = (index-copy.begin())+1;
-    }
+//     vector<int> answer(emergency.size(),0);
+//     vector<int> copy = emergency;
+//     sort(copy.begin(), copy.end(), greater<int>());
+//     for(int i=0;i<emergency.size();i++){
+//         auto index = find(copy.begin(), copy.end(),emergency[i]);
+//         answer[i] = (index-copy.begin())+1;
+//     }
+    auto m = map<int,int>(); //맵 m 선언
+    for(auto& e : emergency){m[e];}
+    auto order = m.size();
+    for(auto& e:m){e.second = order--;}
+    for (auto& e : emergency) { e = m[e]; }
     
-    
-    return answer;
+    return emergency;
 }
