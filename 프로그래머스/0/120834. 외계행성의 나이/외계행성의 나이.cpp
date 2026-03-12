0e5bc6c35c4b15f2a6db9784145880610cf17f1f
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-12 09:22:43 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -5,15 +5,20 @@
 using namespace std;
 
 string solution(int age) {
-    string answer = "";
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
-    if(age==0) return "a";
-    while(age>0){
-        int index = age%10; //일의 자리 추출
-        answer += alpha[index];
-        age/=10; //일의 자리 버리기
-    }
+//     string answer = "";
+//     string alpha = "abcdefghijklmnopqrstuvwxyz";
+//     if(age==0) return "a";
+//     while(age>0){
+//         int index = age%10; //일의 자리 추출
+//         answer += alpha[index];
+//         age/=10; //일의 자리 버리기
+//     }
     
-    reverse(answer.begin(), answer.end());
+//     reverse(answer.begin(), answer.end());
+    
+    string answer = to_string(age);
+    for(auto& v : answer){
+        v+='a'-'0';
+    }
     return answer;
 }
