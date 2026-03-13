b0510e9aefc3a306a356f7281f0775fa0b458f65
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-13 18:05:27 +0900

    [level 0] Title: 모스부호 (1), Time: 0.04 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(string letter) {
+    string answer = "";
+     map<string, string> morse = {
+    {".-", "a"}, {"-...", "b"}, {"-.-.", "c"}, {"-..", "d"}, {".", "e"},
+    {"..-.", "f"}, {"--.", "g"}, {"....", "h"}, {"..", "i"}, {".---", "j"},
+    {"-.-", "k"}, {".-..", "l"}, {"--", "m"}, {"-.", "n"}, {"---", "o"},
+    {".--.", "p"}, {"--.-", "q"}, {".-.", "r"}, {"...", "s"}, {"-", "t"},
+    {"..-", "u"}, {"...-", "v"}, {".--", "w"}, {"-..-", "x"}, {"-.--", "y"},
+    {"--..", "z"}
+    };
+    int start_index = 0;
+    while(true){
+        int end_index = letter.find(' ', start_index); //start_index부터 시작해서 공백 찾기
+        
+        if(end_index == string::npos) {//공백이 없으면(문장 끝났으면)
+            string last_word = letter.substr(start_index);
+            if(!last_word.empty()) //last_word가 비어있지 않으면
+                answer+=morse[last_word]; //morse에서 last_word 키값의 value를 찾아 answer에 붙이기
+            break;
+        }
+        string word = letter.substr(start_index, end_index - start_index);
+        answer+=morse[word];
+        
+        start_index = end_index+1; //시작 인덱스를 끝 인덱스+1 로 설정
+        //다음 공백을 찾기 위해
+            
+    }
+    
+    
+    return answer;
+}
