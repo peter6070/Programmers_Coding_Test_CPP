ce28df596ec614bc6e0957caac5336bed626da52
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-14 14:23:52 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 string solution(string rsp) {
     string answer = "";
-    for(int i=0;i<rsp.size();i++){
-        answer += rsp[i] == '2' ? '0' : (rsp[i] == '0' ? '5' : '2');
+    for(auto it: rsp){
+        answer += it == '2' ? '0' : (it == '0' ? '5' : '2');
     }
     return answer;
 }
