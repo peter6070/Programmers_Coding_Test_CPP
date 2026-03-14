da5f262976e4f00c8912e6621b62dfcf18db26e2
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-14 17:10:43 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int solution(vector<int> numbers, int k) {
-    int answer = 0;
-    int seq = 0;
-    for(int i=0;i<k; i++){
-        if(seq>=numbers.size())
-            seq -= numbers.size();
-        seq+=2;
-    }
-    return seq-1;
+    // int seq = 0;
+    // for(int i=0;i<k; i++){
+    //     if(seq>=numbers.size())
+    //         seq -= numbers.size();
+    //     seq+=2;
+    // }
+    // return seq-1;
+    return numbers[--k * 2 % numbers.size()];
 }
