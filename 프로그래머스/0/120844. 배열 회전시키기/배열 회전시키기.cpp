fbcd3b6113ea79e8589f626d8022169d1bc37420
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-14 17:25:26 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, string direction) {
+    if(direction == "right") 
+        rotate(numbers.begin(),numbers.end()-1, numbers.end());
+    else if(direction == "left") 
+        rotate(numbers.begin(),numbers.begin()+1, numbers.end());
+    return numbers;
+}
