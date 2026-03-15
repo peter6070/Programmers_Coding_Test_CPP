0323789d0ecdf97a591824a6984d9436c4098ea0
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-15 23:12:29 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for(int i=4; i<=n;i++){
-        int count=0;
-        for(int j=1;j<i;j++){
-            if(count >= 2){
+    for (int i = 4; i <= n; i++) {
+        // 2부터 루트 i까지만 확인해도 충분
+        //약수가 1, 자신 뺴고 하나 더 있으면 합성수기 떄문에 더 계산할 필요 X
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
                 answer++;
-                break;
+                break; // 하나라도 나눠떨어지면 break(약수가 또 있다는 뜻)
             }
-            else if(i%j==0) count++;
         }
     }
     return answer;
