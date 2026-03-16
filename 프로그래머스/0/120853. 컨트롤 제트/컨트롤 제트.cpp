e6e848638f76b05349e1cb73cea140ba3ea7f161
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-16 15:52:42 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
@@ -8,12 +8,19 @@ int solution(string s) {
     int answer = 0;
     stringstream ss(s); //문자열 s를 sstream ss에 삽입
     string word;
-    string prevWord;
+    // string prevWord;
+    int lastValue = 0; //직전 값 저장
+    //문자열을 복사해서 저장하는 게 아닌 처음부터 int형으로 저장하여 메모리 절약
     while(ss >> word){ //ss에서 공백을 기준으로 문자를 하나씩 꺼내옴
-        if(word == "Z") answer -= stoi(prevWord);
+        if(word == "Z") 
+            // answer -= stoi(prevWord);
+            // 이미 정수로 바꿨던 값만 빼기 ( stoi 중복 호출 X )
+            answer -= lastValue;
         else{
-            answer += stoi(word);
-            prevWord = word;
+            // answer += stoi(word);
+            // prevWord = word;
+            lastValue = stoi(word);
+            answer += lastValue;
         }
     }
     
