ee99606d09e35b4f69d1f3379220172b055f296f
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-17 13:41:36 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -6,17 +6,31 @@ using namespace std;
 
 
 vector<int> solution(int n) {
+//     vector<int> answer;
+//     int k = 2;
+//     while(k<=n){
+//         if(n%k==0){
+//             n/=k;
+//             if(n%k!=0) answer.push_back(k++);
+//         }
+//         else{
+//             k++;
+//         }
+//     }
+    
+//     return answer;
+    
     vector<int> answer;
-    int k = 2;
-    while(k<=n){
-        if(n%k==0){
-            n/=k;
-            if(n%k!=0) answer.push_back(k++);
-        }
-        else{
-            k++;
+    // n의 제곱근까지만 루프
+    for (int k = 2; k * k <= n; k++) {
+        if (n % k == 0) {
+            answer.push_back(k);
+            while (n % k == 0) n /= k; // k가 n에서 완전히 빠질 때까지 나눔
         }
     }
+    // 루프가 끝났는데 n이 1보다 크다면 , 남은 n은 마지막 소인수
+    if (n > 1) answer.push_back(n);
     
     return answer;
+
 }
