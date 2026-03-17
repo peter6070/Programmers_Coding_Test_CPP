9d96fefa6a13c8ae62a3379c9ddd03f2beb89350
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-17 14:40:14 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> array, int n) {
     int answer = 0;
-    for(int v : array)
-        if(v==n) answer++;
+    // for(int v : array)
+    //     if(v==n) answer++;
+    answer = count(array.begin(), array.end(), n); // n의 개수 반환
     return answer;
 }
