470977ebb70373783a91056a8b3225cfd46a27a2
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-19 16:02:29 +0900

    [level 0] Title: A로 B 만들기, Time: 0.06 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string before, string after) {
+    int answer = 0;
+    sort(before.begin(), before.end());
+    sort(after.begin(), after.end());
+    answer = (before==after) ? 1 : 0;
+    return answer;
+}
