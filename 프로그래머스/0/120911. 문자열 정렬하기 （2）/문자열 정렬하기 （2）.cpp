b2e60a8420334501f493ae782687f065eb6f29b3
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-19 13:50:47 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -7,8 +7,7 @@ using namespace std;
 string solution(string my_string) {
     string answer = "";
     for(auto c: my_string){
-        if(c >= 'A' && c<= 'Z') c+=32;
-        answer+=c;
+        answer += tolower(c);
     }
     sort(answer.begin(), answer.end());
     return answer;
