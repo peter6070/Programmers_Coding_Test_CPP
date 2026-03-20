f4822d98f5d5053a00799ff29ac7dc3529611ec9
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-20 13:32:20 +0900

    [level 0] Title: k의 개수, Time: 1.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.cpp
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -6,11 +6,8 @@ using namespace std;
 int solution(int i, int j, int k) {
     int answer = 0;
     for(int n=i;n<=j;n++){
-        int a = n;
-        while(a>0){
-            if(a%10==k) answer++;
-            a/=10;
-        }
+        for(int m=n;m>0;m/=10)
+            if(m%10==k) answer++;
     }
     return answer;
 }
