7f617a9871f16d1236dcd0d4b4d1e097a6ec9fd4
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-21 22:17:15 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
@@ -8,12 +8,13 @@ vector<string> solution(string my_str, int n) {
     vector<string> answer;
 
     for(int i=0;i<my_str.size();i+=n){
-        string str = "";
-        for(int j=i;j<i+n;j++){
-            if(my_str.size() <= j) break;
-            else str += my_str[j];
-        }
-        answer.push_back(str);
+        // string str = "";
+        // for(int j=i;j<i+n;j++){
+        //     if(my_str.size() <= j) break;
+        //     else str += my_str[j];
+        // }
+        // answer.push_back(str);
+        answer.push_back(my_str.substr(i,n));
     }
     
     
