ada1a74ef5b6a12a53aa7d67b9909f12b81d4f39
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-21 22:05:55 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    int n;
+    for(int i : array){
+        n=i;
+        while(n>0){
+            if(n%10==7) answer++;
+            n/=10;
+        }
+    }
+    return answer;
+}
