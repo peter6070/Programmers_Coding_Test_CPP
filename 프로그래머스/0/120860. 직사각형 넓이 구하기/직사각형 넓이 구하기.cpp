d18e95a43770109e00c3949834835c6d7cb3b748
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-22 23:47:33 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> dots) {
+    int answer=0;
+    int maxX = dots[0][0], minX = dots[0][0];
+    int maxY = dots[0][1], minY = dots[0][1];
+    
+    for(int i = 1; i < dots.size(); i++) { // 0번은 이미 넣었으니 1번부터 돌아도 됩니다.
+        // maxX 찾기
+        if(dots[i][0] > maxX) maxX = dots[i][0];
+        // minX 찾기 (else if 보다는 각각 독립적으로 체크하는 게 더 안전합니다!)
+        if(dots[i][0] < minX) minX = dots[i][0];
+        
+        // maxY 찾기
+        if(dots[i][1] > maxY) maxY = dots[i][1];
+        // minY 찾기
+        if(dots[i][1] < minY) minY = dots[i][1];
+    }
+    
+    answer = (maxX - minX) * (maxY-minY);
+    return answer;
+}
