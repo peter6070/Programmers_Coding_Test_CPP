c9b0e89a14749bde4dca307c12d2ed0b2059f43c
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-23 15:44:34 +0900

    [level 0] Title: 안전지대, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.cpp
+++ 프로그래머스/0/120866. 안전지대/안전지대.cpp
@@ -6,20 +6,21 @@ using namespace std;
 
 int solution(vector<vector<int>> board) {
     int answer=0;
-    vector<vector<int>> copy = board;
-    int n = copy.size();
+    int n = board.size();
+    // vector<vector<int>> copy[n][n]={0};
+    
     
     int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1}; // 행(Row)의 변화량
     int dc[] = {-1, 0, 1, -1, 1, -1, 0, 1}; // 열(Column)의 변화량
     
     for(int r=0;r<n;r++){
         for(int c=0;c<n;c++){
-            if(board[r][c]==1){
+            if(board[r][c]==1){ //1은 지뢰매설지역
                 for(int i=0;i<8;i++){
                 int nr = r+dr[i];
                 int nc = c+dc[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
-                    copy[nr][nc] = 1;
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n && board[nr][nc]==0){
+                    board[nr][nc] = 2; //위험지역은 2로 표시
                 }    
             }
             }
@@ -28,7 +29,7 @@ int solution(vector<vector<int>> board) {
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(copy[i][j]==0) answer++;
+            if(board[i][j]==0) answer++;
         }
     }
     
