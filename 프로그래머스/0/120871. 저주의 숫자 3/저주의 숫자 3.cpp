caa0158177a1113ac3ab2b2e4692448b938e5756
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-24 14:33:22 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool hasThree(int n) {
+    while (n > 0) {
+        if (n % 10 == 3) return true;
+        n /= 10;
+    }
+    return false;
+}
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1;i<=n;i++){
+        answer++;
+        if(answer%3==0||hasThree(answer)){
+            while(answer%3==0||hasThree(answer)){
+                answer++;
+            }
+        }
+    }    
+    
+    return answer;
+}
