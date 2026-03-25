2af5995695f70292325e2c3bdf2de9e24975e093
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-25 21:50:34 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numlist, int n) {
+
+    sort(numlist.begin(), numlist.end(), [n](int a, int b){
+       int distA = abs(n-a);
+        int distB = abs(n-b);
+        
+        if(distA == distB){
+            return a > b;
+        }
+        return distA < distB;
+    });
+    
+    return numlist;
+}
