be68384a35a0d53862678fda5506f13196be4d70
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-26 23:22:40 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -5,25 +5,33 @@
 using namespace std;
 
 string solution(string bin1, string bin2) {
-    //자릿수 맞추기
-    if(bin1.length() < bin2.length()){
-       bin1 = string(bin2.size() - bin1.size(), '0') + bin1;
-    }
-    else{
-       bin2 = string(bin1.size() - bin2.size(), '0') + bin2;
-    }
+//     //자릿수 맞추기
+//     if(bin1.length() < bin2.length()){
+//        bin1 = string(bin2.size() - bin1.size(), '0') + bin1;
+//     }
+//     else{
+//        bin2 = string(bin1.size() - bin2.size(), '0') + bin2;
+//     }
     
-    string answer = "";
-    int carry=0;
+//     string answer = "";
+//     int carry=0;
     
-    for(int i=bin1.size()-1; i>=0;i--){
-        int result = (bin1[i]-'0') + (bin2[i]-'0') + carry;
-        answer += to_string(result%2);
-        carry = result / 2;
-    }
+//     for(int i=bin1.size()-1; i>=0;i--){
+//         int result = (bin1[i]-'0') + (bin2[i]-'0') + carry;
+//         answer += to_string(result%2);
+//         carry = result / 2;
+//     }
     
-    if(carry!=0) answer+=to_string(carry);
-    reverse(answer.begin(), answer.end());
+//     if(carry!=0) answer+=to_string(carry);
+//     reverse(answer.begin(), answer.end());
     
-    return answer;
+    int a=0, b=0;
+    for(auto ch : bin1) a = a<<1 | ch-48;
+    for(auto ch : bin2) b = b<<1 | ch-48;
+    
+    string ret;
+    for(int n = a+b;n;n>>=1) ret = char(n % 2 + 48) + ret;
+    
+    
+    return ret.empty() ? "0" : ret;
 }
