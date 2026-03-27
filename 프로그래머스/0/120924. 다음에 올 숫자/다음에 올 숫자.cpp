5a1ad52038009ec682238cbc72d41560820eb027
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-27 22:26:20 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> common) {
+    if(common[1] - common[0] == common[2] - common[1]) {
+        int d = common[1] - common[0];
+        return common.back() + d;
+    }
+    else{
+        int r = common[1] / common[0];
+        return common.back() * r;
+    }
+    
+}
