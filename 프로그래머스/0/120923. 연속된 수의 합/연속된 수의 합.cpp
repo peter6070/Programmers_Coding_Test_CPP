521c53d912baf6a480e4abd639ac089a93463387
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-03-27 22:06:59 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 vector<int> solution(int num, int total) {
     vector<int> answer;
-    int mid=total/num;
+//     int mid=total/num;
     
-    for(int i=-(num-1)/2;i<num;i++){
-        answer.emplace_back(mid+i);
-        if(answer.size()==num) break;
+//     for(int i=-(num-1)/2;i<num;i++){
+//         answer.emplace_back(mid+i);
+//         if(answer.size()==num) break;
+//     }
+    int sum = (total - (num * (num-1) / 2)) / num;
+    //num * (num-1) / 2 -> 덤으로 가져갈 수(첫번째 숫자에서 num까지 1씩 증가했을 때 그 합)
+    //전체(total)에서 덤을 뺀 값을 원래 인원 수(num)만큼 나누기
+    
+    for(int i=0;i<num;i++){
+        answer.emplace_back(sum+i);
     }
     
     return answer;
