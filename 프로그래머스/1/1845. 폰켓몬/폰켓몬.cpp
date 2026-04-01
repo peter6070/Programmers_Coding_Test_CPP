0e96bc01a590c4e860d70ed42f9cb44b3e8e5eca
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-04-01 09:02:15 +0900

    [level 1] Title: 폰켓몬, Time: 1.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
     map<int,int> list;
     for(auto i : nums) list[i]++;
+
     
-    if(list.size() > nums.size()/2) answer = nums.size()/2;
-    else answer = list.size();
-    
-    return answer;
+    return min(list.size(), nums.size()/2);
 }
