097f39c46e4c5cfe00613043bdb7366e235885f6
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-04-04 21:46:42 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(auto c : commands){
+       vector<int> copy;
+        for(int i=c[0]-1; i<c[1];i++){
+            copy.emplace_back(array[i]);
+        }
+        sort(copy.begin(), copy.end());
+        answer.emplace_back(copy[c[2]-1]);
+    }
+    
+    return answer;
+}
