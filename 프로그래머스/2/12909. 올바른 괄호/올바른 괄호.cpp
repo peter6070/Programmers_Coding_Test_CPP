e88524793e58883141e9efe98e4672629498e8d0
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-04-11 20:59:23 +0900

    [level 2] Title: 올바른 괄호, Time: 0.52 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,21 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> stk;
+
+    for(auto c : s){
+        if(c=='(') stk.push(c);
+        else if(c==')'){
+            if(stk.empty()) return false;
+            stk.pop();
+        }
+    }
+
+    return stk.empty();
+}
