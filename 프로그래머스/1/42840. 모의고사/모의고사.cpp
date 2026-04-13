ea828b4ae2be6d44354d4a54a3b12969151c45fb
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-04-13 13:16:17 +0900

    [level 1] Title: 모의고사, Time: 0.11 ms, Memory: 4.32 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    int a[] = {1,2,3,4,5};
-    int b[] = {2,1,2,3,2,4,2,5};
-    int c[] = {3,3,1,1,2,2,4,4,5,5};
-    int f=0,s=0,t=0;
+    vector<int> a = {1,2,3,4,5};
+    vector<int> b = {2,1,2,3,2,4,2,5};
+    vector<int> c = {3,3,1,1,2,2,4,4,5,5};
+    //맞춘 개수 배열
+     vector<int> result(3);
     
     for(int i=0;i<answers.size();i++){
-        if(answers[i] == a[i%5]) f++;
-        if(answers[i] == b[i%8]) s++;
-        if(answers[i] == c[i%10]) t++;
+        if(answers[i] == a[i%a.size()]) result[0]++;
+        if(answers[i] == b[i%b.size()]) result[1]++;
+        if(answers[i] == c[i%c.size()]) result[2]++;
     }
     
-    int maxScore = max({f, s, t});
-    if (f == maxScore) answer.push_back(1);
-    if (s == maxScore) answer.push_back(2);
-    if (t == maxScore) answer.push_back(3);
+    int maxScore = *max_element(result.begin(), result.end());
+    for(int i=0;i<result.size();i++){
+        if(result[i] == maxScore) answer.emplace_back(i+1);
+    }
         
-    
     return answer;
 }
