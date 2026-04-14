540a4a22d96e3b83f6cb64f663ff05d604a081cb
peter6070 <97502163+peter6070@users.noreply.github.com>
2026-04-14 12:57:31 +0900

    [level 2] Title: 소수 찾기, Time: 0.92 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -14,6 +14,24 @@ bool isPrime(int n){
     return true;
 }
 
+bool next_permutation1(string &numbers){
+    for(int i=numbers.size()-1;i>0;i--){
+        if(numbers[i-1] < numbers[i]){
+            auto target = numbers[i-1];
+            for(int j=numbers.size()-1;j>0;j--){
+                if(target < numbers[j]){
+                    swap(numbers[i-1], numbers[j]);
+                    break;
+                }    
+            }
+            reverse(numbers.begin() + i, numbers.end());
+            return true;
+        }
+    }
+    reverse(numbers.begin(), numbers.end());
+    return false;
+}
+
 int solution(string numbers) {
     int answer = 0;
     set<int> s;
@@ -24,7 +42,8 @@ int solution(string numbers) {
         for (int i = 1; i <= numbers.size(); i++) {
             s.insert(stoi(numbers.substr(0, i)));
         }
-    } while (next_permutation(numbers.begin(), numbers.end()));
+    // } while (next_permutation(numbers.begin(), numbers.end()));
+    } while (next_permutation1(numbers));
     
     //소수 판별별
     for (int e : s) {
